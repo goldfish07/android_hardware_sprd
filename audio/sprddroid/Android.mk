db80c01f75b4eaaee2722260b7390132ca110f2e
judyann63 <34901152+judyann63@users.noreply.github.com>
2017-12-28 16:18:06 +0530

    Update Android.mk

--- audio/sprddroid/Android.mk
+++ audio/sprddroid/Android.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH :=   $(call my-dir)
 
-ifenq $(BOARD_USE_SPRDDROID_AUDIO,true)
+ifenq ($(filter BOARD_USE_SPRDDROID_AUDIO,true)
 include $(call all-makefiles-under,normal)
 endif
 
-ifenq $(BOARD_USE_SPRDDROID_WHALE_AUDIO,true)
+ifenq $( filter BOARD_USE_SPRDDROID_WHALE_AUDIO,true)
 include $(call all makefiles-under,whale)
 endif
