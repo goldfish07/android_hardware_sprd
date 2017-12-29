cfc5cc7973fceb10bfc5dc4222b261cd28ff14d4
judyann63 <34901152+judyann63@users.noreply.github.com>
2017-12-29 14:08:34 +0530

    Update Android.mk

--- audio/Android.mk
+++ audio/Android.mk
@@ -22,13 +22,10 @@ supported_boards := \
 	scx15 \
 	sc8810 \
 
-ifneq (,$(filter SOC_SCX35),true)
+ifneq ($(SOC_SCX35),true)
 include $(call all-makefiles-under,kanas)
 endif 
 
-ifeq (,$(filter SOC_SPRD),true)
+ifneq ($(SOC_SPRD),true)
 include $(call all-makefiles-under,sprddroid)
-else
-ifneq (,$(filter $(supported_boards),$(TARGET_BOARD_PLATFORM)))
-include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
